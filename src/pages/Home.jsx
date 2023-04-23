599c6ce68564987a6c53730d747e1bcec675feec
kasapovicrijad <rikacblyat@gmail.com>
2023-04-23 18:26:26 +0200

    minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>home page</div>
-  )
-}
+    <div>
+      <div>
+      <div class="w-[700px] h-[700px] bg-blue-900 rounded-full float-right my-0 py-0"></div>
+      </div>
+      <div>
+      
+      <h1 className="text-8xl font-bold text-white border-white border-[2px] w-[30rem] ml-[10rem] mt-[15rem] text-center">
+        CHEFS ACADEMY SECRETS
+      </h1>
+      <p className="text-2xl text-white border-white border-[2px] w-[30rem] ml-[10rem] px-[1rem] mt-[5rem] text-left">
+        "Whip Up Culinary Magic: Unleash Your Inner Chef with Our Deliciously
+        Easy Recipes!"
+      </p>
+      
+      
+      
+    </div>
+    </div>
+  );
+};
 
-export default Home
-
-
-
-
+export default Home;
 
 
 
