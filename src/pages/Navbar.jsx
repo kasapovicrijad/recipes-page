3d5e7ae2f180b5c455c5f22fc590194e4c4340e9
kasapovicrijad <rikacblyat@gmail.com>
2023-04-25 23:38:34 +0200

    navbar finished

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>navbar koji ide na svaki page</div>
-  )
-}
+    <div>
+      <nav className="bg-white border-[2px] fixed top-0 w-[100%]">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto">
+          <a className="flex items-center">
+            <span className="self-center text-2xl font-semibold pl-[3rem]">
+              Homechef
+            </span>
+          </a>
 
-export default Navbar
+          <div className="block w-auto">
+            <ul className="font-medium flex flex-row p-4 pr-[3rem] flex-row space-x-5">
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 pl-3 pr-4 text-black rounded"
+                  
+                >
+                  RECIPES
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 pl-3 pr-4 text-black rounded"
+                >
+                  ABOUT
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#"
+                  className="block py-2 pl-3 pr-4 text-black rounded "
+                >
+                  ADD A RECIPE
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+   
+</div>
+  
+  );
+};
+
+export default Navbar;
